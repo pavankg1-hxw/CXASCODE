51faef642d719b938779a77a938e63dc8eb125ab
pavankg1-hxw <pavankalyang1@hexaware.com>
2026-06-24 17:20:41 +0530

    Update provider configuration to use environment variables

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "genesyscloud" {
-    oauthclient_id = "9b693e09-c9f3-4fd6-9239-cf7c02142306"
-    oauthclient_secret = "G40C__XqUzcnuuCXmLVhqQjtloHDwSiXcBop4SNxvzjFN1OhIbinCF2BLdCw"
-    aws_region    = "us-west-2"
+    GENESYSCLOUD_CLIENT_ID = "9b693e09-c9f3-4fd6-9239-cf7c02142306"
+    GENESYSCLOUD_CLIENT_SECRET = "G40C__XqUzcnuuCXmLVhqQjtloHDwSiXcBop4SNxvzjFN1OhIbinCF2BLdCw"
+    GENESYSCLOUD_REGION = "us-west-2"
 }
 
 
