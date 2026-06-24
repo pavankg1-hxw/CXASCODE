f6ed09113a76bf7c9eb0aab9e8805e03e9ea80fc
pavankg1-hxw <pavankalyang1@hexaware.com>
2026-06-24 17:17:26 +0530

    Update genesyscloud provider configuration
    Added oauthclient_id, oauthclient_secret, and aws_region to the genesyscloud provider configuration.

--- provider.tf
+++ provider.tf
@@ -8,7 +8,9 @@ terraform {
 }
 
 provider "genesyscloud" {
-    
+    oauthclient_id = "9b693e09-c9f3-4fd6-9239-cf7c02142306"
+    oauthclient_secret = "G40C__XqUzcnuuCXmLVhqQjtloHDwSiXcBop4SNxvzjFN1OhIbinCF2BLdCw"
+    aws_region    = "us-west-2"
 }
 
 
