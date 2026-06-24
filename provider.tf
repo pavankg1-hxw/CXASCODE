dbd807801b65e6f428d099ca5481b335030b6dd7
pavankg1-hxw <pavankalyang1@hexaware.com>
2026-06-24 16:55:12 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+      genesyscloud = {
+        source = "mypurecloud/genesyscloud"
+        version = "1.81.0"
+      }
+    }
+}
+
+provider "genesyscloud" {
+    
+}
+
+
+
