2cc66ea7b81f8e1fb3e578d59cc02dd6ec271594
pavankg1-hxw <pavankalyang1@hexaware.com>
2026-06-25 13:21:07 +0530

    Clean up genesyscloud provider configuration
    Removed sensitive environment variables from provider configuration.

--- provider.tf
+++ provider.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 provider "genesyscloud" {
-    GENESYSCLOUD_CLIENT_ID = "9b693e09-c9f3-4fd6-9239-cf7c02142306"
-    GENESYSCLOUD_CLIENT_SECRET = "G40C__XqUzcnuuCXmLVhqQjtloHDwSiXcBop4SNxvzjFN1OhIbinCF2BLdCw"
-    GENESYSCLOUD_REGION = "us-west-2"
+
 }
 
 
