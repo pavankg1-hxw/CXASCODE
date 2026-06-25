3617d37c6216eadb07c59cc2fe143759dab777ae
pavankg1-hxw <pavankalyang1@hexaware.com>
2026-06-25 16:41:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
-resource "genesyscloud_routing_wrapupcode" "win" {
-  name        = "Win"
-  description = "Win test description"
+resource "genesyscloud_tf_export" "gc-datatable" {
+    directory               = "./genesyscloud-export"
+    compress                = false
+    export_as_hcl           = true
+    ignore_cyclic_deps      = true
+    enable_dependency_resolution  = true
+    split_files_by_resource = true
+    include_state_file      = false
+    log_permission_errors   = true
+    include_filter_resources =[
+        "genesyscloud_routing_wrapupcode::^Booking Cancel$"
+    ]
 }
