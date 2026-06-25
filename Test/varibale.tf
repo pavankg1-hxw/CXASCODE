1db297263057a3556bf23b318a9d4059ce0c5914
pavankg1-hxw <pavankalyang1@hexaware.com>
2026-06-25 16:23:48 +0530

    Replace TEST_GENESYSCLOUD variables with GENESYSCLOUD

--- Test/varibale.tf
+++ Test/varibale.tf
@@ -1,20 +1,14 @@
-variable "TEST_GENESYSCLOUD_OAUTHCLIENT_ID" {
-    description = ""
+variable "GENESYSCLOUD_CLIENT_ID" {
     type = string
     sensitive = true
-    default = "9b693e09-c9f3-4fd6-9239-cf7c02142306"
 }
 
-variable "TEST_GENESYSCLOUD_OAUTHCLIENT_SECRET" {
-    description = ""
+variable "GENESYSCLOUD_CLIENT_SECRET" {
     type = string
     sensitive = true
-    default = "G40C__XqUzcnuuCXmLVhqQjtloHDwSiXcBop4SNxvzjFN1OhIbinCF2BLdCw"
 }
 
-variable "TEST_GENESYSCLOUD_REGION" {
-    description = ""
+variable "GENESYSCLOUD_REGION" {
     type = string
     sensitive = true
-    default = "us-west-2"
 }
